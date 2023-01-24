b05fa69d4a046e8c81871e0f68cf6f3d2625e7ab
ramlifting <ramlifting@rubens-mbp.home>
2023-01-24 14:22:18 -0500

    Module 1 Java Class

---
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author ramlifting
+ */
+public class HelloWorld {
+    // another method 
+    public int sum (int a, int b) {
+        return a + b; 
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
