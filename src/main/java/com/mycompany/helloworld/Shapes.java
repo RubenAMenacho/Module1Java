efe9e2d66b747890cdfa605c1aea9c175e5d4fdf
ramlifting <ramlifting@rubens-mbp.home>
2023-01-25 12:57:19 -0500

    Second Commit

---
+++ src/main/java/com/mycompany/helloworld/Shapes.java
@@ -0,0 +1,24 @@
+/*
+ * Exercise 2.18 Shapes
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author ramlifting
+ */
+public class Shapes 
+{
+    public static void main(String[] args) {
+        System.out.println("********       ***         *         *     "); 
+        System.out.println("*      *     *     *      ***       * *    "); 
+        System.out.println("*      *    *       *    *****     *   *   ");
+        System.out.println("*      *    *       *      *      *     * .");
+        System.out.println("*      *    *       *      *     *       * ");
+        System.out.println("*      *    *       *      *      *     *  ");
+        System.out.println("*      *    *       *      *       *   *   ");
+        System.out.println("*      *     *     *       *        * *    ");
+        System.out.println("********       ***         *         *     ");
+                 
+    }
+}
